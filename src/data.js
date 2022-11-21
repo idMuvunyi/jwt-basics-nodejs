81b249acf4c6dc87de0283fc7473e750c513653b
Muvunyi Idrissa <muvunyiiddy@gmail.com>
2022-11-21 07:11:29 +0200

    implement basic user registration endpoint

---
+++ src/data.js
@@ -0,0 +1,7 @@
+exports.database = [
+  {
+    id: 1,
+    email: "muvunyiiddy@gmail.com",
+    password: "$2b$10$HYdj1TVdhJdU4CJGxRQnOeQocAjHp.MblDLcyey9aHflvob0hOQ2a",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const { verify } = require("jsonwebtoken");
 const { hash, compare } = require("bcrypt");
-
+const { database } = require("./data");
 // Register a user
 
 const server = express();
@@ -28,3 +28,27 @@ server.use(express.urlencoded({ extended: true }));
 server.listen(process.env.PORT, () =>
   console.log(`Server running at port ${process.env.PORT}`)
 );
+
+// endpoint routes (placed here for now)
+server.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    // if user does exist
+    const user = database.find((user) => user.email === email);
+    if (user) throw new Error("User already exists");
+
+    // if user does not exist, hash the password
+    const hashedPassword = await hash(password, 10);
+    // insert user in the database
+    database.push({
+      id: database.length,
+      email,
+      password: hashedPassword,
+    });
+    res.send({ message: "User created successfully", data: database });
+  } catch (error) {
+    res.send({
+      error: `${error.message}`,
+    });
+  }
+});
